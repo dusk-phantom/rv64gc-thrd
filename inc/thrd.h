60adc9c6571598ff8699059a61939b4d5dcbab20
wangfiox <wangfiox@hotmail.com>
2024-04-06 01:14:41 +0800

    modified:   inc/thrd.h

--- inc/thrd.h
+++ inc/thrd.h
@@ -31,57 +31,6 @@ int thrd_create(long num);
 
 #define futex(uaddr, futex_op, val, timeout, uaddr2, val3) syscall(SYS_futex, uaddr, futex_op, val, timeout, uaddr2, val3)
 
-// static inline __attribute__((always_inline)) int thrd_create(int num)
-// {
-//     int id = 0;
-//     asm volatile(
-//         "mv a0, %1\n"
-//         "jal ra, __thrd_create_utils\n"
-//         "mv %0, a0\n"
-//         : "=r"(id)
-//         : "r"(num)
-//         :);
-//     return id;
-// }
-
-// static inline __attribute__((always_inline)) int thrd_create(int num)
-// {
-//     long s0_sp_difference;
-//     __asm__ volatile(
-//         "sub %0, s0, sp\n" // 计算s0与sp的差值
-//         : "=r"(s0_sp_difference) // 输出到变量
-//         :
-//         : "memory");
-
-//     int id = 0;
-//     int* p_num = &num;
-//     while (num > 0) {
-//         if (id == 0) {
-//             id = _thrd_create(s0_sp_difference);
-//             if (id == -1) {
-//                 exit(-1);
-//             } else if (id == 0) {
-//                 // 主线程
-//                 num--;
-//             } else {
-//                 while (*p_num > 1) {
-//                     __asm__ volatile("nop");
-//                 }
-//                 return id;
-//             }
-//         }
-//     }
-//     return id;
-// }
-
-// #define CREATE(ID, NUM)                      \
-//     for (int ___i = 0; ___i < NUM; ___i++) { \
-//         ID = thrd_create();                  \
-//         if (ID < 0) {                        \
-//             exit(-1);                        \
-//         }                                    \
-//     }
-
 /**
  * @brief 等待之前所有创建的子线程结束, 在这之后只有主线程继续执行, 失败返回-1
  *
