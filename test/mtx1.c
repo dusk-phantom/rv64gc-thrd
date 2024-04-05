2fdace3a2f389938bc602fdb4e09ecb87563dfa5
wangfiox <wangfiox@hotmail.com>
2024-04-05 20:47:30 +0800

    fix test | update docs

--- test/mtx1.c
+++ test/mtx1.c
@@ -1,41 +1,41 @@
-#include <assert.h>
+/**
+ * @file main.c
+ * @author your name (you@domain.com)
+ * @brief
+ * @version 0.1
+ * @date 2024-03-26
+ *
+ * @copyright Copyright (c) 2024
+ *
+ */
+
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+
 #include <syscall.h>
 #include <unistd.h>
 
 #include "thrd.h"
 
-// TODO: 测试mtx
-int main()
+int main(void)
 {
-    int sum = 0;
+    // fork();
 
-    int mtx = mtx_create();
+    int _sum = 0;
+    int* sum = &_sum;
 
-    int id = thrd_create(1);
-    switch (id) {
-    case -1:
-        abort();
-    case 0: // 主线程 ，因为 主线程的 tid == 0
-        for (int i = 0; i < 100; i++) {
-            mtx_lock(mtx);
-            sum++;
-            mtx_unlock(mtx);
-        }
-        break;
-    case 1: // 子线程，因为子线程的 tid == 0
-        for (int i = 0; i < 100; i++) {
-            mtx_lock(mtx);
-            sum++;
-            mtx_unlock(mtx);
-        }
-        break;
+    int mtx1 = mtx_create();
+
+    thrd_create(1);
+
+    for (int i = 0; i < 100; i++) {
+        mtx_lock(mtx1);
+        (*sum)++;
+        mtx_unlock(mtx1);
     }
 
     thrd_join();
 
-    printf("sum=%d\n", sum);
-
-    return sum;
+    printf("sum=%d\n", _sum);
 }
